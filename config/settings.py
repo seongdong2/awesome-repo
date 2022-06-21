275b4f6ddc9b89bd91c3b34aa2e36e4a4df9a139
home <chosd1123@naver.com>
2022-06-21 23:58:43 +0900

    Serializers

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,9 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -132,3 +134,9 @@ MEDIA_URL = "/media/"
 # Auth
 
 AUTH_USER_MODEL = "users.User"
+
+# 아래 내용 지우면 DEFAULT_AUTO_FIELD 오류 발생함!
+# pk가 없이 생성된 객체들에게 자동으로 pk 먹이는 것이라고 함.
+# 추후 모델에 primary_key=True 먹여줘야할 듯
+ 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
